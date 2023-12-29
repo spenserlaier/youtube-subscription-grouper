c05a2f231d3996031e037ca55bee247f2b1f2f9e
Spenser Laier <spenserlaier@gmail.com>
2023-12-29 15:22:43 -0500

    initialize oauth

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+//import Link from 'next/link';
+
+export default function Login() {
+  return (
+      <div>
+          <h1> this is the homepage huzzah</h1>
+      </div>
+  )
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,32 @@
 import Image from 'next/image'
+//import Link from 'next/link';
 
 export default function Login() {
+    const {google} = require('googleapis');
+    const REDIRECT_URL = "http://localhost:3000/home"
+
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.OAUTH_CLIENT_ID,
+      process.env.OAUTH_CLIENT_SECRET,
+      REDIRECT_URL
+    );
+    // generate a url that asks permissions for youtube subscription scope
+    const scopes = [
+      'https://www.googleapis.com/auth/youtube.readonly'
+    ];
+    const url = oauth2Client.generateAuthUrl({
+      // 'online' (default) or 'offline' (gets refresh_token)
+      access_type: 'offline',
+      // If you only need one scope you can pass it as a string
+      scope: scopes
+    });
   return (
       <div>
           this is a default login div
           oh snap
+          <a href={url}>
+              authenticate with google
+          </a>
       </div>
   )
 }
